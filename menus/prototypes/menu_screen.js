b7b571aa597fe6afb98a609225d2321c91e28a37
Drithyl <hugo.bujan@gmail.com>
2021-02-01 22:01:01 -0500

    Added a setDisplayText method

--- menus/prototypes/menu_screen.js
+++ menus/prototypes/menu_screen.js
@@ -8,9 +8,15 @@ function MenuScreen(displayText, behaviour)
     assert.isStringOrThrow(displayText);
     assert.isFunctionOrThrow(behaviour);
 
-    const _displayText = displayText;
+    var _displayText = displayText;
     const _behaviour = behaviour;
 
     this.getDisplayText = () => _displayText;
+    this.setDisplayText = (displayStr) =>
+    {
+        assert.isStringOrThrow(displayStr);
+        _displayText = displayStr;
+    };
+
     this.invokeBehaviour = (...args) => _behaviour(...args);
 }
