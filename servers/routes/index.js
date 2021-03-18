6927abe4f2e1ce70b631d5c61dcfd411dd32f411
Hugo Bujan Prevot <hugo.bujan@gmail.com>
2021-03-18 18:52:09 -0400

    Moved oauth2 url to config file

--- servers/routes/index.js
+++ servers/routes/index.js
@@ -1,7 +1,10 @@
 
+const config = require("../../config/config.json");
 
 
 exports.set = (expressApp) => 
 {
-    expressApp.get("/", (req, res) => res.render("home_screen.ejs"));
+    expressApp.get("/", (req, res) => res.render("home_screen.ejs", {
+        oAuth2Url: config.oAuth2Url
+    }));
 };
