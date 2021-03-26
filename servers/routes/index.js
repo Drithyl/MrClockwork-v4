6e34b8aaf7cc430a3c78a8dc8b91b75fd8106b95
Hugo Buján Prévot <hugo.bujan@gmail.com>
2021-03-26 10:48:28 -0400

    Fixed oAuth2Url reference

--- servers/routes/index.js
+++ servers/routes/index.js
@@ -5,6 +5,6 @@ const config = require("../../config/config.json");
 exports.set = (expressApp) => 
 {
     expressApp.get("/", (req, res) => res.render("home_screen.ejs", {
-        discordRedirectUri: config.discordRedirectUri
+        oAuth2Url: config.oAuth2Url
     }));
 };
