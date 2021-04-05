8be667a9737e778bb707c5bec8179447a1cfb953
Hugo Buján Prévot <hugo.bujan@gmail.com>
2021-04-05 02:23:26 -0400

    Added command to set log level

---
+++ discord/command_modules/set_log_level.js
@@ -0,0 +1,35 @@
+
+const log = require("../../logger.js");
+const assert = require("../../asserter.js");
+const Command = require("../prototypes/command.js");
+const CommandData = require("../prototypes/command_data.js");
+const commandPermissions = require("../command_permissions.js");
+
+const commandData = new CommandData("SET_LOG_LEVEL");
+
+module.exports = SetLogLevelCommand;
+
+function SetLogLevelCommand()
+{
+    const setLogLevelCommand = new Command(commandData);
+
+    setLogLevelCommand.addBehaviour(_behaviour);
+
+    setLogLevelCommand.addRequirements(
+        commandPermissions.assertMemberIsDev
+    );
+
+    return setLogLevelCommand;
+}
+
+function _behaviour(commandContext)
+{
+    const commandArguments = commandContext.getCommandArgumentsArray();
+    const logLevelArg = +commandArguments[0];
+
+    if (assert.isInteger(logLevelArg) === false || logLevelArg < 0 || logLevelArg > 2)
+        return commandContext.respondToCommand(`Log level is an integer from 0 to 2`);
+
+    log.setLogLevel(logLevelArg);
+    return commandContext.respondToCommand(`Log level set to ${logLevelArg}.`);
+}
