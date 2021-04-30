ae7d8b4d412fd295d4392a2987ecdd6b1b764239
Drithyl <hugo.bujan@gmail.com>
2021-04-29 23:35:46 -0400

    Added delete game dev command

---
+++ discord/command_modules/delete_game_by_dm.js
@@ -0,0 +1,44 @@
+
+const log = require("../../logger.js");
+const Command = require("../prototypes/command.js");
+const CommandData = require("../prototypes/command_data.js");
+const commandPermissions = require("../command_permissions.js");
+const ongoingGamesStore = require("../../games/ongoing_games_store.js");
+
+const commandData = new CommandData("DELETE_GAME_BY_DM");
+
+module.exports = DeleteGameByDmCommand;
+
+function DeleteGameByDmCommand()
+{
+    const deleteGameByDmCommand = new Command(commandData);
+
+    deleteGameByDmCommand.addBehaviour(_behaviour);
+
+    deleteGameByDmCommand.addRequirements(
+        commandPermissions.assertMemberIsDev
+    );
+
+    return deleteGameByDmCommand;
+}
+
+function _behaviour(commandContext)
+{
+    const commandArgumentsArray = commandContext.getCommandArgumentsArray();
+    const nameOfGameToRepair = commandArgumentsArray[0];
+    var gameObject;
+    
+
+    if (ongoingGamesStore.hasOngoingGameByName(nameOfGameToRepair) === false)
+        return commandContext.respondToCommand(`No game found with this name.`);
+
+    gameObject = ongoingGamesStore.getOngoingGameByName(nameOfGameToRepair);
+
+    
+    return commandContext.respondToCommand(`Deleting game...`)
+    .then(() => gameObject.deleteGame())
+    .then(() => gameObject.deleteRole())
+    .then(() => gameObject.deleteChannel())
+    .then(() => commandContext.respondToCommand(`The game was successfully deleted.`))
+    .then(() => log.general(log.getLeanLevel(), `${gameObject.getName()} and its role and channel were deleted successfully.`));
+}
