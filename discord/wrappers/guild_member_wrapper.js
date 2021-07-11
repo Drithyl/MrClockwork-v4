43de30c0aa67e09df43ad9a993011c35d9edf5c1
Drithyl <hugo.bujan@gmail.com>
2021-07-11 12:46:16 -0400

    Added nickname and current name methods

--- discord/wrappers/guild_member_wrapper.js
+++ discord/wrappers/guild_member_wrapper.js
@@ -12,6 +12,8 @@ function GuildMemberWrapper(discordJsGuildMemberObject, guildWrapper)
 
   this.getId = () => discordJsGuildMemberObject.id;
   this.getUsername = () => discordJsGuildMemberObject.user.username;
+  this.getNickname = () => discordJsGuildMemberObject.nickname;
+  this.getNameInGuild = () => (this.getNickname() != null) ? this.getNickname() : this.getUsername();
   this.getGuildId = () => _guildWrapper.getGuildId();
 
   this.hasRole = (discordRoleId) => _discordJsGuildMemberObject.roles.cache.get(discordRoleId) != null;
