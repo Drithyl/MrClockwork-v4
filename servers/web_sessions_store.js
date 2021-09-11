25439680c7e096963c3e077298d29d074f679461
Drithyl <hugo.bujan@gmail.com>
2021-09-11 19:53:20 -0400

    Refactored to use new WebSession prototype

--- servers/web_sessions_store.js
+++ servers/web_sessions_store.js
@@ -1,87 +1,37 @@
 
 const url = require("url");
-const uuidv4 = require("uuid").v4;
+const WebSession = require("./prototypes/web_session.js");
 
 const _sessions = {};
 
 module.exports.createSession = (userId) =>
 {
-    const token = uuidv4();
-    _sessions[token] = {userId, token};
-    return token;
+    const session = new WebSession(userId);
+    const sessionId = session.getSessionId();
+
+    _sessions[sessionId] = session;
+    return session;
 };
 
-module.exports.storeData = (token, data) =>
+module.exports.getSession = (sessionId) =>
 {
-    if (_sessions[token] == null)
-        throw new Error(`User session with token ${token} does not exist.`);
-
-    _sessions[token].data = data;
+    return _sessions[sessionId];
 };
 
-module.exports.storeResult = (token, result) =>
-{
-    if (_sessions[token] == null)
-        throw new Error(`User session with token ${token} does not exist.`);
-
-    _sessions[token].result = result;
-};
-
-module.exports.doesSessionExist = (token) =>
-{
-    return _sessions[token] != null && _sessions[token].token === token;
-};
-
-module.exports.removeSession = (token) =>
-{
-    delete _sessions[token];
-};
-
-module.exports.getSessionUserId = (token) =>
-{
-    if (_sessions[token] == null)
-        return null;
-
-    return _sessions[token].userId;  
-};
-
-module.exports.getSessionData = (token) =>
-{
-    if (_sessions[token] == null)
-        return null;
-        
-    return _sessions[token].data;  
-};
-
-module.exports.getSessionResult = (token) =>
-{
-    if (_sessions[token] == null)
-        return null;
-        
-    return _sessions[token].result;  
-};
-
-module.exports.redirectToResult = (expressRes, token, resultMessage) =>
-{
-    if (_sessions[token] == null)
-        throw new Error(`User session with token ${token} does not exist.`);
-
-    exports.storeResult(token, resultMessage);
-    expressRes.redirect("/result?token=" + sessionToken);
-    return Promise.resolve();
-};
-
-module.exports.extractSessionParamsFromUrl = (reqUrl) =>
+module.exports.getSessionFromUrlParams = (reqUrl) =>
 {
     const urlObject = url.parse(reqUrl, true);
-    const token = urlObject.query.token;
-    const userId = exports.getSessionUserId(token);
-
-    return { userId, token };
+    const sessionId = urlObject.query.sessionId;
+    const session = exports.getSession(sessionId);
+    return session;
 };
 
-module.exports.isSessionValid = (authenticationParams) =>
+module.exports.doesSessionExist = (sessionId) =>
 {
-    const token = authenticationParams.token;
-    return exports.doesSessionExist(token);
+    return _sessions[sessionId] != null && _sessions[sessionId].getSessionId() === sessionId;
+};
+
+module.exports.removeSession = (sessionId) =>
+{
+    delete _sessions[sessionId];
 };
