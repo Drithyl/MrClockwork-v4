d9a3633e9cab12edb37916c30b3cd87d0a340f61
Drithyl <hugo.bujan@gmail.com>
2021-10-06 14:34:54 -0400

    Added member and guild count to home page

--- servers/routes/index.js
+++ servers/routes/index.js
@@ -1,6 +1,8 @@
 
 const config = require("../../config/config.json");
 const expressServer = require("../express_server.js");
+const botClient = require("../../discord/wrappers/bot_client_wrapper.js");
+const guildStore = require("../../discord/guild_store.js");
 
 
 exports.set = (expressApp) => 
@@ -10,6 +12,18 @@ exports.set = (expressApp) =>
         if (req.secure === false && expressServer.isHttpsAvailable() === true)
             return res.redirect(config.fullSecureUrl);
 
-        res.render("home_screen.ejs");
+        var guildCount = 0;
+        var memberCount = 0;
+
+        guildStore.forEachGuild((guildWrapper) =>
+        {
+            guildCount++;
+            memberCount += guildWrapper.getMemberCount();
+        });
+
+        res.render("home_screen.ejs", {
+            guildCount,
+            memberCount
+        });
     });
 };
