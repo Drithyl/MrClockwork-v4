9685dac3f987af3771d33071b8d3cfec97dd4ae5
Drithyl <hugo.bujan@gmail.com>
2021-11-07 13:13:06 -0500

    Moved fetching of game-related data to user_home_screen.js, to keep /login as purely a login route

--- servers/routes/login.js
+++ servers/routes/login.js
@@ -4,7 +4,6 @@ const log = require("../../logger.js");
 const oauth2 = require("../discord_oauth2.js");
 const config = require("../../config/config.json");
 const webSessionsStore = require("../web_sessions_store.js");
-const ongoingGames = require("../../games/ongoing_games_store.js");
 
 
 exports.set = (expressApp) => 
@@ -24,8 +23,6 @@ exports.set = (expressApp) =>
         oauth2.authenticate(urlObject)
         .then((userInfo) => 
         {
-            var playedGames;
-            var organizedGames;
             const userId = userInfo.id;
 
             if (userId == null)
@@ -36,51 +33,8 @@ exports.set = (expressApp) =>
             res.cookie("sessionId", sessionId);
             log.general(log.getVerboseLevel(), `Request authenticated! userId: ${userId}, sessionId: ${sessionId}, cookie set`);
 
-            playedGames = _getPlayedGamesData(userId);
-            organizedGames = _getOrganizedGamesData(userId);
-            console.log(organizedGames);
-            
-            session.storeSessionData({
-                sessionId, 
-                userId,
-                playedGames,
-                organizedGames
-            });
-
             session.redirectTo("user_home_screen", res);
         })
         .catch((err) => res.render("results_screen.ejs", { result: `Error occurred: ${err.message}` }));
     });
 };
-
-
-function _getPlayedGamesData(userId)
-{
-    const games = ongoingGames.getGamesWhereUserIsPlayer(userId);
-    return games.map(_extractGameInfo);
-}
-
-function _getOrganizedGamesData(userId)
-{
-    const games = ongoingGames.getGamesWhereUserIsOrganizer(userId);
-    return games.map(_extractGameInfo);
-}
-
-function _extractGameInfo(game)
-{
-    const statusData = game.getLastKnownStatus();
-
-    return {
-        name: game.getName(),
-        guild: game.getGuild()?.getName(),
-        server: game.getServer()?.getName(),
-        organizer: game.getOrganizer()?.getNameInGuild(),
-        organizerId: game.getOrganizerId(),
-        ip: game.getIp(),
-        port: game.getPort(),
-        status: statusData?.getStatus(),
-        turnNumber: statusData?.getTurnNumber(),
-        timeLeft: statusData?.getTimeLeft()?.printTimeLeftShort(),
-        isPaused: statusData?.isPaused()
-    };
-}
