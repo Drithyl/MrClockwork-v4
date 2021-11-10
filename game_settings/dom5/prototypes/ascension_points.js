14141f89a663fbd9a0789d7b846aae2151cccfc7
Drithyl <hugo.bujan@gmail.com>
2021-11-10 17:32:44 -0500

    Added thrones check in _validateInputFormatOrThrow()

--- game_settings/dom5/prototypes/ascension_points.js
+++ game_settings/dom5/prototypes/ascension_points.js
@@ -1,4 +1,5 @@
 
+const assert = require("../../../asserter.js");
 const GameSetting = require("../../prototypes/game_setting.js");
 const SemanticError = require("../../../errors/custom_errors.js").SemanticError;
 
@@ -6,16 +7,20 @@ const key = "ascensionPoints";
 
 module.exports = AscensionPoints;
 
-function AscensionPoints()
+function AscensionPoints(parentGameObject)
 {
     var _value;
+    const _parentGame = parentGameObject;
 
     this.getValue = () => _value;
     this.getReadableValue = () => _value;
     
     this.setValue = (input) =>
     {
-        const validatedValue = _validateInputFormatOrThrow(input);
+        const settingsObject = _parentGame.getSettingsObject();
+        const thronesSetting = settingsObject.getThronesSetting();
+        const thrones = thronesSetting.getValue();
+        const validatedValue = _validateInputFormatOrThrow(input, thrones);
 
         _value = validatedValue;
     };
@@ -35,7 +40,7 @@ function AscensionPoints()
         return [`--requiredap`, value];
     };
 
-    function _validateInputFormatOrThrow(input)
+    function _validateInputFormatOrThrow(input, thrones)
     {
         if (AscensionPoints.prototype.isExpectedFormat(input) === false)
             throw new SemanticError(`Invalid value format for ascension points.`);
@@ -44,6 +49,9 @@ function AscensionPoints()
 
         if (points <= 0 || points > 80)
             throw new SemanticError(`Ascension Points required must be between 1 and 80`);
+
+        if (assert.isArray(thrones) === true && thrones[0] + (thrones[1] * 2) + (thrones[2] * 3) < points)
+            throw new SemanticError(`Sum of the throne points must be at least as high as the ascension points required`);
 
         return points;
     }
