99b6953c295ee70ce0fc628366985a515f216b6e
Drithyl <hugo.bujan@gmail.com>
2021-12-07 23:24:52 -0500

    Added new events

--- discord/discord_events.js
+++ discord/discord_events.js
@@ -2,6 +2,9 @@
 const log = require("../logger.js");
 const onJoinedGuild = require("./discord_event_handlers/on_joined_guild.js");
 const onLeftGuild = require("./discord_event_handlers/on_left_guild.js");
+const onGuildMemberJoined = require("./discord_event_handlers/on_guild_member_joined.js");
+const onGuildMemberRemoved = require("./discord_event_handlers/on_guild_member_removed.js");
+const onGuildMemberUpdated = require("./discord_event_handlers/on_guild_member_updated.js");
 const onMessageReceived = require("./discord_event_handlers/on_message_received.js");
 const onCommandInteractionReceived = require("./discord_event_handlers/on_command_interaction_received.js");
 const onReactionAdded = require("./discord_event_handlers/on_reaction_added.js");
@@ -13,6 +16,9 @@ exports.startListening = () =>
     log.general(log.getNormalLevel(), "Adding discord event handlers...");
     onJoinedGuild.startListening();
     onLeftGuild.startListening();
+    onGuildMemberJoined.startListening();
+    onGuildMemberRemoved.startListening();
+    onGuildMemberUpdated.startListening();
     onMessageReceived.startListening();
     onCommandInteractionReceived.startListening();
     onReactionAdded.startListening();
