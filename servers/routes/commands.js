dabe9bdacdbd1307a73309a05671683d99e6acfa
Drithyl <hugo.bujan@gmail.com>
2021-12-13 00:10:14 -0500

    Tweaked to fit the new nfs drive where all maps and mods are hosted, rather than on each separate server

---
+++ servers/routes/commands.js
@@ -0,0 +1,11 @@
+
+const commandStore = require("../../discord/command_store.js");
+
+
+exports.set = (expressApp) => 
+{
+    expressApp.get("/commands", (req, res) =>
+    {
+        
+    });
+};
