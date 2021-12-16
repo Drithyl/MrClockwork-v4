be03e16a0b31d05b71b18ba316b775c8628a08bb
Drithyl <hugo.bujan@gmail.com>
2021-12-15 19:08:23 -0500

    Fixed functions referencing argument instead of the stored _discordJsGuildMemberObject

--- discord/wrappers/guild_member_wrapper.js
+++ discord/wrappers/guild_member_wrapper.js
@@ -13,16 +13,16 @@ function GuildMemberWrapper(discordJsGuildMemberObject, guildWrapper)
 
     this.getGuildWrapper = () => _guildWrapper;
   
-    this.getId = () => discordJsGuildMemberObject.id;
-    this.getUsername = () => discordJsGuildMemberObject.user.username;
-    this.getNickname = () => discordJsGuildMemberObject.nickname;
-    this.getNameInGuild = () => (this.getNickname() != null) ? this.getNickname() : this.getUsername();
+    this.getId = () => _discordJsGuildMemberObject.id;
+    this.getUsername = () => _discordJsGuildMemberObject.user.username;
+    this.getNickname = () => _discordJsGuildMemberObject.nickname;
+    this.getNameInGuild = () => this.getNickname() ?? this.getUsername();
     this.getGuildId = () => _guildWrapper.getId();
 
     this.hasRole = (discordRoleId) => _discordJsGuildMemberObject.roles.cache.get(discordRoleId) != null;
     this.addRole = (discordRoleObject) => _discordJsGuildMemberObject.roles.add(discordRoleObject);
     this.removeRole = (discordRoleObject) => _discordJsGuildMemberObject.roles.remove(discordRoleObject);
     this.fetchRole = (discordRoleId) => _discordJsGuildMemberObject.roles.fetch(discordRoleId, { cache: true });
-    this.getHighestDiscordRolePosition = () => discordJsGuildMemberObject.roles.highest.position;
+    this.getHighestDiscordRolePosition = () => _discordJsGuildMemberObject.roles.highest.position;
     this.sendMessage = (payload) => payload.send(_discordJsGuildMemberObject);
 }
