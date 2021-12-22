7f1025bb5f8c110ddf2b2f617b63f987e2c52712
Drithyl <hugo.bujan@gmail.com>
2021-12-21 19:29:04 -0500

    Fixed new pipes being created for each logged message, eventually leading to an EMFILE error from too many file handles open

--- logger.js
+++ logger.js
@@ -1,6 +1,7 @@
 
 const fs = require("fs");
 const path = require("path");
+const stream = require("stream");
 const assert = require("./asserter.js");
 const config = require("./config/config.json");
 
@@ -13,6 +14,12 @@ const VERBOSE_LEVEL = 2;
 var currentLogLevel = config.defaultLogLevel;
 var isLoggingToFile = true;
 
+var dayOfMonth = new Date().getDate();
+var generalWriteStream;
+var errorWriteStream;
+var uploadWriteStream;
+
+_updateStreamPaths();
 
 module.exports.getLeanLevel = () => LEAN_LEVEL;
 module.exports.getNormalLevel = () => NORMAL_LEVEL;
@@ -41,7 +48,7 @@ module.exports.toggleLogToFile = () =>
 module.exports.general = (logLevel, header, ...data) =>
 {
     var logStr = _log(logLevel, header, ...data);
-    _logToFile(logStr, "general.txt");
+    _logToFile(logStr, generalWriteStream);
 };
 
 module.exports.command = (logLevel, commandContext) =>
@@ -64,19 +71,19 @@ module.exports.command = (logLevel, commandContext) =>
     logStr += ` with args [${args.join(", ")}]`;
 
     var logStr = _log(logLevel, logStr);
-    _logToFile(logStr, "general.txt");
+    _logToFile(logStr, generalWriteStream);
 };
 
 module.exports.error = (logLevel, header, ...data) =>
 {
     var logStr = _log(logLevel, header, ...data);
-    _logToFile(logStr, "error.txt");
+    _logToFile(logStr, errorWriteStream);
 };
 
 module.exports.upload = (logLevel, header, ...data) =>
 {
     var logStr = _log(logLevel, header, ...data);
-    _logToFile(logStr, "upload.txt");
+    _logToFile(logStr, uploadWriteStream);
 };
 
 
@@ -100,14 +107,45 @@ function _log(logLevel, header, ...data)
     return logStr;
 }
 
-function _logToFile(logStr, filename)
+function _logToFile(logStr, writeStream)
 {
     if (isLoggingToFile === false)
         return;
 
+    _updateStreamPaths();
+    writeStream.write(logStr);
+}
+
+function _updateStreamPaths()
+{
     const date = new Date();
-    const logPath = path.resolve(BASE_LOG_PATH, `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}-${filename}`);
-    fs.createWriteStream(logPath, { flags: "a" }).write(logStr);
+    const day = date.getDate();
+
+    if (dayOfMonth === day && 
+        assert.isInstanceOfPrototype(generalWriteStream, stream.Writable) === true && 
+        assert.isInstanceOfPrototype(errorWriteStream, stream.Writable) === true && 
+        assert.isInstanceOfPrototype(uploadWriteStream, stream.Writable) === true)
+        return;
+
+    dayOfMonth = day;
+
+    if (generalWriteStream != null && generalWriteStream.destroyed === false)
+        generalWriteStream.destroy();
+
+    if (errorWriteStream != null && errorWriteStream.destroyed === false)
+        errorWriteStream.destroy();
+
+    if (uploadWriteStream != null && uploadWriteStream.destroyed === false)
+        uploadWriteStream.detroy();
+
+    generalWriteStream = fs.createWriteStream(_getLogPath(date, "general.txt"), { flags: "a", autoClose: true });
+    errorWriteStream = fs.createWriteStream(_getLogPath(date, "error.txt"), { flags: "a", autoClose: true });
+    uploadWriteStream = fs.createWriteStream(_getLogPath(date, "upload.txt"), { flags: "a", autoClose: true });
+}
+
+function _getLogPath(date, filename)
+{
+    return path.resolve(BASE_LOG_PATH, `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}-${filename}`);
 }
 
 function _getTimestamp()
