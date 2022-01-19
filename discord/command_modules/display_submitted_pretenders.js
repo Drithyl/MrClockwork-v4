06126d762b86e2f229eee6b2ea86c9a77921077e
Drithyl <hugo.bujan@gmail.com>
2022-01-19 18:02:01 -0500

    Refactored formatting and added total count of submitted and of claimed prtenders

--- discord/command_modules/display_submitted_pretenders.js
+++ discord/command_modules/display_submitted_pretenders.js
@@ -1,5 +1,4 @@
 
-const log = require("../../logger.js");
 const assert = require("../../asserter.js");
 const Command = require("../prototypes/command.js");
 const CommandData = require("../prototypes/command_data.js");
@@ -26,35 +25,51 @@ function DisplaySubmittedPretendersCommand()
 
 function _behaviour(commandContext)
 {
-    var formattedListAsString = "";
     var game = commandContext.getGameTargetedByCommand();
     
     return game.fetchSubmittedNations()
     .then((listAsArray) => 
     {
-        listAsArray.forEach((submittedPretender, index) => 
-        {
-            if (submittedPretender.isHuman === true)
-                formattedListAsString += `${submittedPretender.nationNbr}. `.width(5) + `${_formatSubmittedPretenderLine(submittedPretender, commandContext)}`;
-        });
+        console.log(listAsArray);
+        const humanPretenders = listAsArray.filter((pretender) => pretender.isHuman === true);
+        const formattedString = _formatSubmittedPretenders(humanPretenders);
 
-        if (formattedListAsString === "")
+        if (humanPretenders.length <= 0)
             return commandContext.respondToCommand(new MessagePayload(`There are no submitted pretenders.`));
 
-        return commandContext.respondToCommand(new MessagePayload(formattedListAsString.toBox(), "", true, "```"));
+        return commandContext.respondToCommand(new MessagePayload(formattedString, "", true, "```"));
     });
 }
 
-function _formatSubmittedPretenderLine(submittedPretender)
+function _formatSubmittedPretenders(humanPretenderList)
 {
-    const fullNationName = submittedPretender.fullName;
-    const pretenderOwnerMember = submittedPretender.owner;
+    var totalClaimed = 0;
+    var countString = "";
+    var formattedString = "";
 
-    if (assert.isString(pretenderOwnerMember) === true)
-        return `${fullNationName.width(40)} ${pretenderOwnerMember}\n`;
+    humanPretenderList.forEach((pretender) =>
+    {
+        const fullNationName = pretender.fullName;
+        const pretenderOwnerMember = pretender.owner;
 
-    if (pretenderOwnerMember != null)
-        return `${fullNationName.width(40)} ${pretenderOwnerMember.getUsername()}\n`;
+        const indexString = `${pretender.nationNbr}. `.width(5);
+        var pretenderStr = fullNationName.width(40);
 
-    else return `${fullNationName}\n`;
+        // If claimed, increment our count
+        if (pretenderOwnerMember != null)
+            totalClaimed++;
+
+        // If .owner property is a string, just add it
+        if (assert.isString(pretenderOwnerMember) === true)
+            pretenderStr += `${pretenderOwnerMember}`;
+    
+        // If owner property would be a GuildMemberWrapper, use getUsername()
+        else if (pretenderOwnerMember != null)
+            pretenderStr += `${fullNationName.width(40)} ${pretenderOwnerMember.getUsername()}`;
+
+        formattedString += indexString + pretenderStr + "\n";
+    });
+
+    countString = `Total Submitted: ${humanPretenderList.length}\nTotal Claimed: ${totalClaimed}\n`;
+    return countString + formattedString.toBox();
 }
