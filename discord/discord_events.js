59558c7b42ec5db613c3748db2ae8c1379dc0b4a
Drithyl <hugo.bujan@gmail.com>
2022-02-16 21:36:37 -0500

    Added onReactionRemoved handler

--- discord/discord_events.js
+++ discord/discord_events.js
@@ -9,6 +9,7 @@ const onGuildMemberUpdated = require("./discord_event_handlers/on_guild_member_u
 const onMessageReceived = require("./discord_event_handlers/on_message_received.js");
 const onCommandInteractionReceived = require("./discord_event_handlers/on_command_interaction_received.js");
 const onReactionAdded = require("./discord_event_handlers/on_reaction_added.js");
+const onReactionRemoved = require("./discord_event_handlers/on_reaction_removed.js");
 const onChannelDeleted = require("./discord_event_handlers/on_channel_deleted.js");
 const onRoleDeleted = require("./discord_event_handlers/on_role_deleted.js");
 
@@ -24,6 +25,7 @@ exports.startListening = () =>
     onMessageReceived.startListening();
     onCommandInteractionReceived.startListening();
     onReactionAdded.startListening();
+    onReactionRemoved.startListening();
     onChannelDeleted.startListening();
     onRoleDeleted.startListening();
     log.general(log.getNormalLevel(), "Discord event handlers added.");
