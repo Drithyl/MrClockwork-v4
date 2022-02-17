50f6d6500863022e69b3df632c000f28a9f30650
Drithyl <hugo.bujan@gmail.com>
2022-02-16 21:34:56 -0500

    Remove code relating to pending channels

--- discord/discord_event_handlers/on_channel_deleted.js
+++ discord/discord_event_handlers/on_channel_deleted.js
@@ -2,7 +2,6 @@
 const log = require("../../logger.js");
 const guildStore = require("../guild_store.js");
 const botClientWrapper = require("../wrappers/bot_client_wrapper.js");
-const pendingChannelsStore = require("../pending_game_channel_store.js");
 
 
 exports.startListening = () =>
@@ -17,12 +16,6 @@ exports.startListening = () =>
            guildWrapper.clearData(channel.id)
            .then(() => log.general(log.getNormalLevel(), `Bot Channel ${channel.name} was deleted; cleared its data as well.`))
            .catch((err) => log.error(log.getLeanLevel(), `ERROR CLEARING DATA IN CHANNEL ${channel.name} IN GUILD ${channel.guild.id}`, err));
-        }
-
-        else
-        {
-            pendingChannelsStore.removeGameChannelPendingHosting(channel.id);
-            log.general(log.getNormalLevel(), `Channel ${channel.name} was deleted; removed from pending list.`);
         }
     });
 };
