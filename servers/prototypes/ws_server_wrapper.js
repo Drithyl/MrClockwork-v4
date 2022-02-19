ba045f406bbd810722fbc1528c8beb1db24818c4
Drithyl <hugo.bujan@gmail.com>
2022-02-19 16:12:53 -0500

    Wrappers for ws objects to replace socket.io ones

---
+++ servers/prototypes/ws_server_wrapper.js
@@ -0,0 +1,109 @@
+
+
+const log = require("../../logger.js");
+const { WebSocketServer } = require("ws");
+const assert = require("../../asserter.js");
+
+module.exports = WebSocketServerWrapper;
+
+
+function WebSocketServerWrapper(port)
+{
+    const _port = port ?? 8080;
+    const _wss = new WebSocketServer({ port: _port });
+    const interval = setInterval(_ping.bind(null, _wss), 30000);
+
+    var _onListeningHandler;
+    var _onServerClosedHandler;
+    var _onSocketConnectionHandler;
+    var _onServerErrorHandler;
+
+    this.getPort = () => _port;
+
+    this.onListening = (handler) =>
+    {
+        if (assert.isFunction(handler) === false)
+            throw new Error(`onListening() handler must be a function; received ${typeof handler} instead!`);
+
+        _onListeningHandler = handler;
+    };
+
+    this.onServerClosed = (handler) =>
+    {
+        if (assert.isFunction(handler) === false)
+            throw new Error(`onServerClosed() handler must be a function; received ${typeof handler} instead!`);
+
+        _onServerClosedHandler = handler;
+    };
+
+    this.onSocketConnection = (handler) =>
+    {
+        if (assert.isFunction(handler) === false)
+            throw new Error(`onSocketConnection() handler must be a function; received ${typeof handler} instead!`);
+
+        _onSocketConnectionHandler = handler;
+    };
+
+    this.onServerError = (handler) =>
+    {
+        if (assert.isFunction(handler) === false)
+            throw new Error(`onServerError() handler must be a function; received ${typeof handler} instead!`);
+
+        _onServerErrorHandler = handler;
+    };
+
+    _wss.on("listening", (err) =>
+    {
+        if (assert.isFunction(_onListeningHandler) === true)
+            _onListeningHandler();
+    });
+
+    _wss.on("close", () =>
+    {
+        if (assert.isFunction(_onServerClosedHandler) === true)
+            _onServerClosedHandler();
+            
+        clearInterval(interval);
+    });
+
+    _wss.on("connection", (ws, req) =>
+    {
+        if (assert.isFunction(_onSocketConnectionHandler) === true)
+            _onSocketConnectionHandler(ws, req);
+
+        ws.isAlive = true;
+        ws.ip = req.socket.remoteAddress;
+        ws.on("pong", _heartbeat);
+    });
+
+    _wss.on("error", (err) =>
+    {
+        if (assert.isFunction(_onServerErrorHandler) === true)
+            _onServerErrorHandler(err);
+    });
+}
+
+// Following recommended implentation:
+// https://github.com/websockets/ws#how-to-detect-and-close-broken-connections
+function _ping(server)
+{
+    server.clients.forEach((ws) =>
+    {
+        if (ws.isAlive === false)
+        {
+            log.error(log.getLeanLevel(), `Connection with socket ${ws.ip} timed out, terminating it...`);
+            return ws.terminate();
+        }
+    
+        ws.isAlive = false;
+        ws.ping();
+        log.time(ws.ip, `${ws.ip} pong, time since ping`);
+    });
+}
+
+function _heartbeat()
+{
+    // 'this' is passed by context to the function on the event listeners
+    this.isAlive = true;
+    log.timeEnd(this.ip);
+}
---
+++ servers/prototypes/ws_wrapper.js
@@ -0,0 +1,243 @@
+
+
+
+const log = require("../../logger.js");
+const assert = require("../../asserter.js");
+const handleDom5Data = require("../../games/dominions5_runtime_data_handler.js");
+const { TimeoutError, SocketResponseError } = require("../../errors/custom_errors");
+
+module.exports = WebSocketWrapper;
+
+function WebSocketWrapper(ws, req)
+{
+    const _ws = ws;
+    const _sentMessages = [];
+    const _eventHandlers = {};
+    const _ip = req.socket.remoteAddress;
+
+    // Handle incoming messages on this socket distributing them
+    // among listening handlers or treating them as responses
+    // from previously sent messages by this socket
+    _ws.on("message", (data) =>
+    {
+        const parsedData = _parse(data);
+        const trigger = parsedData.trigger;
+        const pendingPromise = _sentMessages.find((wsPromise) => trigger === wsPromise.getTrigger());
+        const handler = _eventHandlers[trigger];
+
+        // If we were pending a response, handle it directly in the WebSocketPromise object
+        if (assert.isInstanceOfPrototype(pendingPromise, WebSocketPromise) === true)
+            pendingPromise.handleResponse(parsedData);
+
+        // Otherwise this is a direct message, so handle its data with the registered handler
+        else if (assert.isFunction(handler) === true)
+            handler(parsedData.data, parsedData.expectsResponse);
+
+        else log.error(log.getLeanLevel(), `Received message with unregistered trigger ${trigger}`);
+    });
+
+    this.getId = () => _ip;
+
+    this.close = (code, reason) => _ws.close(code, reason);
+    
+    this.terminate = () => _ws.terminate();
+
+    this.onDisconnect = (fnToCall) => _ws.on("close", fnToCall);
+
+    this.emit = (trigger, data, error = null, expectsResponse = false) => 
+    {
+        const wrappedData = {
+            trigger,
+            data: data,
+            expectsResponse,
+            error: (assert.isObject(error) === true) ? error.stack : error
+        };
+
+        _ws.send( _stringify(wrappedData) );
+    };
+
+    this.emitPromise = (trigger, data, timeout = 60000) =>
+    {
+        return new Promise((resolve, reject) =>
+        {
+            const wsPromise = new WebSocketPromise(trigger, resolve, reject, timeout);
+            this.emit(trigger, data, null, true);
+            _sentMessages.push(wsPromise);
+
+            // Cleanup the promise from the array of sent messages
+            // once it resolves, rejects or timeouts
+            wsPromise.onFinished(() => 
+            {
+                _sentMessages.find((promise, i) =>
+                {
+                    if (promise.getTrigger() === trigger)
+                    {
+                        _sentMessages.splice(i, 1);
+                        return true;
+                    }
+                });
+            });
+        });
+    };
+
+    this.listenTo = (trigger, handler) =>
+    {
+        if (assert.isFunction(handler) === false)
+            throw new Error(`Handler must be a function; received ${typeof handler} instead!`);
+
+        _eventHandlers[trigger] = (data, expectsResponse) =>
+        {
+            // Process data with the given handler
+            Promise.resolve(handler(data))
+            .then((result) =>
+            {
+                // Response to the received message
+                // with resulting data
+                if (expectsResponse === true)
+                    module.exports.emit(trigger, result);
+            })
+            // Respond with an error if an exception was caught
+            .catch((err) => 
+            {
+                if (expectsResponse === true)
+                    this.emit(trigger, null, err)
+            });
+        };
+    };
+
+    
+    this.listenTo("STDIO_DATA", (data) => 
+    {
+        log.general(log.getVerboseLevel(), `${data.name}: ${data.type} data received`, data.data);
+        handleDom5Data(data.name, data.data);
+    });
+
+    this.listenTo("GAME_ERROR", (data) => 
+    {
+        log.error(log.getLeanLevel(), `${data.name} REPORTED GAME ERROR`, data.error);
+        handleDom5Data(data.name, data.error);
+    });
+}
+
+function WebSocketPromise(trigger, resolveFn, rejectFn, timeout)
+{
+    const _trigger = trigger;
+    const _resolveFn = resolveFn;
+    const _rejectFn = rejectFn;
+    const _timeout = timeout ?? 60000;
+
+    var _receivedResponse = false;
+    var _cleanupHandler;
+
+    this.getTrigger = () => _trigger;
+
+    this.onFinished = (cleanupFn) => _cleanupHandler = cleanupFn;
+
+    this.handleResponse = (data) =>
+    {
+        if (_receivedResponse === true)
+            return;
+
+        if (data == null)
+            _settle(_rejectFn.bind(this, new SocketResponseError(`No data packet for this response was received!`)));
+
+        else if (data.error != null)
+            _settle(_rejectFn.bind(this, new SocketResponseError(data.error)));
+
+        else _settle(_resolveFn.bind(this, data.data));
+    };
+
+    // If a timeout was given, start it as the creation of this object
+    if (assert.isInteger(_timeout) === true && _timeout > 0)
+    {
+        setTimeout(function handleTimeout()
+        {
+            if (_receivedResponse === true)
+                return;
+
+            _receivedResponse = true;
+            _settle(_rejectFn.bind(this, new TimeoutError(`Request timed out.`)));
+
+            if (assert.isFunction(_cleanupHandler) === true)
+                _cleanupHandler();
+
+        }, _timeout);
+    }
+
+    function _settle(handlerFn)
+    {
+        if (_receivedResponse === true)
+            return;
+
+        _receivedResponse = true;
+        handlerFn();
+
+        if (assert.isFunction(_cleanupHandler) === true)
+            _cleanupHandler();
+    }
+}
+
+function _parse(data)
+{
+    var parsedData;
+
+    if (Buffer.isBuffer(data) === true)
+        parsedData = _jsonParseWithBufferRevival(data.toString());
+
+    if (assert.isString(data) === true)
+        parsedData = _jsonParseWithBufferRevival(data);
+
+    return parsedData;
+}
+
+// Parse JSON while also checking if there is any nested serialized
+// Buffer value. These appear as objects with a .type === "Buffer" and
+// .data property that contains the actual buffer. If found, return the
+// .data property properly converted to a Buffer object
+function _jsonParseWithBufferRevival(data)
+{
+    return JSON.parse(data, (key, value) =>
+    {
+        if (_isSerializedBuffer(value) === true)
+            return Buffer.from(value.data);
+
+        return value;
+    });
+}
+
+// Checks if an object is in fact a serialized Buffer. These appear as 
+// objects with a .type === "Buffer" and .data property that contains the actual buffer.
+function _isSerializedBuffer(value)
+{
+  return value != null && 
+    typeof value === "object" &&
+    value.type === "Buffer" && 
+    Array.isArray(value.data) === true;
+}
+
+// Stringify that prevents circular references taken from https://antony.fyi/pretty-printing-javascript-objects-as-json/
+function _stringify(data, spacing = 0)
+{
+	var cache = [];
+
+	// custom replacer function gets around the circular reference errors by discarding them
+	var str = JSON.stringify(data, function(key, value)
+	{
+		if (typeof value === "object" && value != null)
+		{
+			// value already found before, discard it
+			if (cache.indexOf(value) !== -1)
+			    return;
+
+			// not found before, store this value for reference
+			cache.push(value);
+		}
+
+		return value;
+
+	}, spacing);
+
+	// enable garbage collection
+	cache = null;
+	return str;
+}
