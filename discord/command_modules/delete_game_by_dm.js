a11a97592d8d54ad61160fa308621c901a24dd4f
Hugo Buján Prévot <hugo.bujan@gmail.com>
2022-03-13 19:53:20 -0400

    Will send a delete request to slave even if game does not exist on master, to remove leftover savedgames files

--- discord/command_modules/delete_game_by_dm.js
+++ discord/command_modules/delete_game_by_dm.js
@@ -23,23 +23,22 @@ function DeleteGameByDmCommand()
     return deleteGameByDmCommand;
 }
 
-function _behaviour(commandContext)
+async function _behaviour(commandContext)
 {
     const commandArgumentsArray = commandContext.getCommandArgumentsArray();
     const nameOfGameToRepair = commandArgumentsArray[0];
-    var gameObject;
-    
-
-    if (ongoingGamesStore.hasOngoingGameByName(nameOfGameToRepair) === false)
-        return commandContext.respondToCommand(new MessagePayload(`No game found with this name.`));
-
-    gameObject = ongoingGamesStore.getOngoingGameByName(nameOfGameToRepair);
+    const gameObject = ongoingGamesStore.getOngoingGameByName(nameOfGameToRepair);
 
     
-    return commandContext.respondToCommand(new MessagePayload(`Deleting game...`))
-    .then(() => gameObject.deleteGame())
-    .then(() => gameObject.deleteRole())
-    .then(() => gameObject.deleteChannel())
-    .then(() => commandContext.respondToCommand(new MessagePayload(`The game was successfully deleted.`)))
-    .then(() => log.general(log.getLeanLevel(), `${gameObject.getName()} and its role and channel were deleted successfully.`));
+    await commandContext.respondToCommand(new MessagePayload(`Deleting game...`));
+
+    if (gameObject == null)
+        await gameObject.emitPromiseWithGameDataToServer("DELETE_GAME", null, 130000);
+
+    else await gameObject.deleteGame();
+
+    await gameObject.deleteRole();
+    await gameObject.deleteChannel();
+    await commandContext.respondToCommand(new MessagePayload(`The game was successfully deleted.`));
+    log.general(log.getLeanLevel(), `${gameObject.getName()} and its role and channel were deleted successfully.`);
 }
