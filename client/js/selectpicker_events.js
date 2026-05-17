decaa9ceb7ae3571717c4e0e8227a1e8a3d7066f
Drithyl <hugo.bujan@gmail.com>
2026-05-17 18:26:32 +0200

    Custom selectpicker behaviour to preserve pick order in mod dropdown

--- client/js/selectpicker_events.js
+++ client/js/selectpicker_events.js
@@ -1,12 +1,30 @@
-$(document).ready(() =>
-{
-    const picker = $('#input_mod');
+$(function () {
+    const modPicker = $('#input_mod');
+    preservePickOrder(modPicker);
 
-    picker.on('loaded.bs.select', selectedFirst);
-    picker.on('change.bs.select', selectedFirst);
+    function preservePickOrder(picker) {
+        const options = picker.find('option').toArray();
 
-    function selectedFirst () {
-        $(this).find('option:selected').prependTo(this);
-        $(this).selectpicker('refresh');
+        picker.on('loaded.bs.select', selectedFirst);
+        picker.on('change.bs.select', selectedFirst);
+
+        function selectedFirst () {
+            // Get the currently selected options
+            const selectedOptions = $(this).find('option:selected');
+
+            // Find the non-selected options among the original list
+            const otherOptions = [...options].filter(o => {
+                return Array.from(selectedOptions).find(so => {
+                    return o.value === so.value;
+                }) == null;
+            });
+
+            // Add the selected options first
+            $(this).prepend(selectedOptions);
+
+            // Then add the non-selected ones
+            $(this).append(otherOptions);
+            $(this).selectpicker('refresh');
+        }
     }
 });
